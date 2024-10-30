361a9b1a165161ff0c090c2c8e70827a8486bfb3
EVMlord <85406816+EVMlord@users.noreply.github.com>
2024-10-30 07:04:06 -0700

    Update bot.ts

--- api/bot.ts
+++ api/bot.ts
@@ -3,27 +3,27 @@ import bot from "../src"; // Import the initialized bot from src/index.ts
 import ipRangeCheck from "ip-range-check";
 import { webhookCallback } from "grammy";
 
-const handleUpdate = webhookCallback(bot, "express");
+const handleUpdate = webhookCallback(bot, "http");
 
 // Define allowed IP ranges (from Telegram documentation) in CIDR notation
 const allowedIPRanges = ["149.154.160.0/20", "91.108.4.0/22"];
 
-function createCompatibleResponse(res: VercelResponse): any {
-  return {
-    ...res,
-    set: (field: string, value: string | string[]) => {
-      res.setHeader(field, value);
-      return res;
-    },
-  };
-}
+// function createCompatibleResponse(res: VercelResponse): any {
+//   return {
+//     ...res,
+//     set: (field: string, value: string | string[]) => {
+//       res.setHeader(field, value);
+//       return res;
+//     },
+//   };
+// }
 
-function createCompatibleRequest(req: VercelRequest): any {
-  return {
-    ...req,
-    header: (name: string) => req.headers[name.toLowerCase()],
-  };
-}
+// function createCompatibleRequest(req: VercelRequest): any {
+//   return {
+//     ...req,
+//     header: (name: string) => req.headers[name.toLowerCase()],
+//   };
+// }
 
 export default async function botHandler(
   req: VercelRequest,
@@ -51,10 +51,11 @@ export default async function botHandler(
   // Process Telegram update if the IP is allowed
   if (req.method === "POST") {
     // Wrap the Vercel request and response to make them compatible with webhookCallback
-    const compatibleReq = createCompatibleRequest(req);
-    const compatibleRes = createCompatibleResponse(res);
+    // const compatibleReq = createCompatibleRequest(req);
+    // const compatibleRes = createCompatibleResponse(res);
 
-    return handleUpdate(compatibleReq, compatibleRes);
+    // return handleUpdate(compatibleReq, compatibleRes);
+    return handleUpdate(req, res);
   } else {
     res.status(405).send("Method Not Allowed");
   }
